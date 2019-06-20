fea0fbc68d6b955250249b1c3d0e375b5a3e1f70
song bingwei <bingwei.song@weesharing.com>
2019-06-20 10:14:15 +0800

    find coupon by sku

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/feign/hystric/EquityServiceH.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/feign/hystric/EquityServiceH.java
@@ -1,7 +1,10 @@
 package com.fengchao.product.aoyi.feign.hystric;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fengchao.product.aoyi.bean.OperaResult;
 import com.fengchao.product.aoyi.feign.EquityService;
+import com.fengchao.product.aoyi.model.AoyiProdIndex;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +15,21 @@ public class EquityServiceH implements EquityService {
         OperaResult result = new OperaResult();
         result.setCode(404);
         result.setMsg("获取促销活动信息失败 " + skuId);
+        return result;
+    }
+
+    @Override
+    public OperaResult selectCouponBySku(AoyiProdIndex bean) {
+        OperaResult result = new OperaResult();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String msg = "";
+        try {
+            msg = objectMapper.writeValueAsString(bean);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        result.setCode(404);
+        result.setMsg("权益服务失败 " + msg);
         return result;
     }
 }
