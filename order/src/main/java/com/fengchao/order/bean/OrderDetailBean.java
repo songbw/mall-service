c5c1ec4e89f9888f66bf1107248d0eab967f774a
lufei <1808319164@qq.com>
2019-07-01 10:24:31 +0800

    add adminorder

--- order/src/main/java/com/fengchao/order/bean/OrderDetailBean.java
+++ order/src/main/java/com/fengchao/order/bean/OrderDetailBean.java
@@ -48,4 +48,25 @@ public class OrderDetailBean {
     private Date paymentAt;
     private String logisticsId;
     private String logisticsContent;
+    private String outTradeNo;
+    private Integer paymentAmount;
+    // 商户号，充值钱包的时候没有
+    private String payee ;
+    // 退款金额，退款时候有
+    private int refundFee;
+    // 支付方式
+    private String payType;
+    // 订单总金额
+    private int paymentTotalFee;
+    // C端个人账号。 表示唯一用户
+    private String payer;
+    // 支付状态 10初始创建订单  1下单成功，等待支付。  2支付中，3超时未支付  4支付失败  5支付成功  11支付成功，记账也成功   12支付成功，记账失败  14退款失败，15订单已退款
+    private int payStatus;
+    // 1支付，2充值，3退款，4提现
+    private int payOrderCategory ;
+    private Integer couponId;
+    private String couponCode;
+    private Float couponDiscount;
+    private Integer promotionId;
+    private BigDecimal promotionDiscount;
 }
