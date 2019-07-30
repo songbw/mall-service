8aeb9c0c03c00db4b3f345b7c426f8ec6741b908
lufei <1808319164@qq.com>
2019-07-30 17:36:32 +0800

    add Serializable

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/bean/CategoryQueryBean.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/bean/CategoryQueryBean.java
@@ -3,9 +3,11 @@ package com.fengchao.product.aoyi.bean;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class CategoryQueryBean {
+public class CategoryQueryBean implements Serializable {
     private Integer id;
     private String name;
 }
