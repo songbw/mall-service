11189554c9230aad4ea34923ef349b46d3b68bdb
song bingwei <bingwei.song@weesharing.com>
2019-08-02 13:50:16 +0800

    fix hystric

--- order/src/main/java/com/fengchao/order/feign/hystric/ProductServiceH.java
+++ order/src/main/java/com/fengchao/order/feign/hystric/ProductServiceH.java
@@ -1,6 +1,5 @@
 package com.fengchao.order.feign.hystric;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fengchao.order.bean.OperaResult;
 import com.fengchao.order.feign.ProductService;
 import org.springframework.stereotype.Component;
@@ -11,17 +10,7 @@ import java.util.List;
 public class ProductServiceH implements ProductService {
     @Override
     public OperaResult find(String id) {
-        OperaResult result = new OperaResult();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String msg = "";
-//        try {
-//            msg = objectMapper.writeValueAsString(queryBean);
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
-        result.setCode(404);
-        result.setMsg("获取商品信息失败 " + id);
-        return result;
+        return HystrixDefaultFallback.defaultFallback();
     }
 
     @Override
