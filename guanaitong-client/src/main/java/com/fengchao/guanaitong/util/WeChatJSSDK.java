deab5813509120cb4a1c7245e49647230740f0c9
Clark <1554711952@qq.com>
2019-08-05 21:26:57 +0800

    guanaitong-client: add wechat sing

---
+++ guanaitong-client/src/main/java/com/fengchao/guanaitong/util/WeChatJSSDK.java
@@ -0,0 +1,15 @@
+package com.fengchao.guanaitong.util;
+
+public class WeChatJSSDK {
+    public static final String JS_SDK_APPID = "wx060d200a5a513106";
+    public static final String JS_SDK_APP_SECRET = "d800e152aa51b2031c87e77a227209d9";
+    public static final String JS_SDK_URL_PREFIX = "https://api.weixin.qq.com/cgi-bin/";
+
+    public static final String JS_SDK_TOKEN_KEY = "JSSDKToken";
+    public static final String JS_SDK_TOKEN_PATH = "token";
+
+    public static final String JS_SDK_TICKET_KEY = "JSSDKTicket";
+    public static final String JS_SDK_GET_TICKET_PATH = "ticket/getticket";
+
+}
+
