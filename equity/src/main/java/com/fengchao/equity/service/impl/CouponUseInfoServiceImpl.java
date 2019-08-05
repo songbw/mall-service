141137f30d9dae14022da5871b668b09420919b7
lufei <1808319164@qq.com>
2019-08-05 10:10:50 +0800

    modify type

--- equity/src/main/java/com/fengchao/equity/service/impl/CouponUseInfoServiceImpl.java
+++ equity/src/main/java/com/fengchao/equity/service/impl/CouponUseInfoServiceImpl.java
@@ -85,7 +85,6 @@ public class CouponUseInfoServiceImpl implements CouponUseInfoService {
             couponUseInfo.setCollectedTime(new Date());
             couponUseInfo.setUserCouponCode(user_coupon_code);
             couponUseInfo.setUrl(url);
-            couponUseInfo.setType(1);
             num = mapper.insertSelective(couponUseInfo);
             couponUseInfoBean.setUserCouponCode(user_coupon_code);
         }else if(coupon.getCollectType() == 4){
