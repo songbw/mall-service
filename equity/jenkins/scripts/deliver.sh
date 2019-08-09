2cb101bcfde6086c2e267b37dd083b0b92b6a973
tom <jingtao416@sina.com>
2019-08-09 16:48:14 +0800

    v

--- equity/jenkins/scripts/deliver.sh
+++ equity/jenkins/scripts/deliver.sh
@@ -1,35 +1,3 @@
-#!/usr/bin/expect
+#!/bin/sh
 
-set user smartadmin
-set passwd Smartautotech@123
-set host 192.168.200.122
-set port 22
-set src_dir ./statistics/target/
-set tag_dir /data/server/statistics/userapps/
-set name statistics-0.0.1-SNAPSHOT.jar
-set tmp_dir /tmp
-
-spawn sh -c " scp -P $port -r $src_dir$name $user@$host:$tag_dir$tmp_dir"
-expect "password:"
-send "${passwd}\n"
-set timeout 30000
-expect "$ "
-
-spawn ssh $user@$host -p $port
-expect "password:"
-send "${passwd}\n"
-expect "]$ "
-send "cd $tag_dir\n"
-expect "]$ "
-send "rm $name\n"
-expect "]$ "
-send "mv $tag_dir$tmp_dir/$name .\n"
-expect "]$ "
-
-send "cd ../bin/\n"
-expect "]$ "
-
-send "./stop.sh\n"
-expect "]$ "
-send "./start.sh\n"
-expect "]$ "
+echo 'success'
