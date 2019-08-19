07a0a4031949ec9465858c74f55ec97f49b72271
song bingwei <bingwei.song@weesharing.com>
2019-08-19 18:28:47 +0800

    fix product

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/ProductHandle.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/ProductHandle.java
@@ -4,15 +4,17 @@ import com.fengchao.product.aoyi.model.AoyiProdIndexX;
 
 public class ProductHandle {
     public static AoyiProdIndexX updateImage(AoyiProdIndexX aoyiProdIndexX) {
-        String imageUrl = aoyiProdIndexX.getImagesUrl();
-        if (aoyiProdIndexX.getImage() == null && "".equals(aoyiProdIndexX.getImage()) && imageUrl != null && (!"".equals(imageUrl))) {
-            String image = "";
-            if (imageUrl.indexOf("/") == 0) {
-                image = CosUtil.iWalletUrlT + imageUrl.split(":")[0];
-            } else {
-                image = CosUtil.baseAoyiProdUrl + imageUrl.split(":")[0];
+        if (aoyiProdIndexX.getImage() == null || "".equals(aoyiProdIndexX.getImage())) {
+            String imageUrl = aoyiProdIndexX.getImagesUrl();
+            if (imageUrl != null && (!"".equals(imageUrl))) {
+                String image = "";
+                if (imageUrl.indexOf("/") == 0) {
+                    image = CosUtil.iWalletUrlT + imageUrl.split(":")[0];
+                } else {
+                    image = CosUtil.baseAoyiProdUrl + imageUrl.split(":")[0];
+                }
+                aoyiProdIndexX.setImage(image);
             }
-            aoyiProdIndexX.setImage(image);
         }
         return aoyiProdIndexX;
     }
