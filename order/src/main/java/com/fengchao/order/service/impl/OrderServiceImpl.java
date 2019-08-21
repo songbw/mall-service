e031673634ab0d414a7b1a8bc8dfec91b07e460d
tom <jingtao416@sina.com>
2019-08-21 18:17:19 +0800

    v

--- order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
@@ -251,6 +251,7 @@ public class OrderServiceImpl implements OrderService {
                     adminOrderDao.updateAoyiIdByTradeNo(subOrderT.getAoyiId(), subOrderT.getOrderNo());
                 }
             });
+            logger.info("创建订单 OrderServiceImpl#add2 返回orderMerchantBeans:{}", JSONUtil.toJsonString(orderMerchantBeans));
             operaResult.getData().put("result", orderMerchantBeans) ;
 
             logger.info("创建订单 OrderServiceImpl#add2 返回:{}", JSONUtil.toJsonString(operaResult));
