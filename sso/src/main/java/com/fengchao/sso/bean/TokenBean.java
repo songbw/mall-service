3c6097b35710d0f5af52344afb3a4860cbfdfb5e
lufei <1808319164@qq.com>
2019-08-23 15:53:14 +0800

    add isNewUser

---
+++ sso/src/main/java/com/fengchao/sso/bean/TokenBean.java
@@ -0,0 +1,54 @@
+package com.fengchao.sso.bean;
+
+public class TokenBean {
+
+    private String openId;
+
+    private String token;
+
+    private Integer expireDate;
+
+    private String thirdToken;
+
+    private boolean isNewUser;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId == null ? null : openId.trim();
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token == null ? null : token.trim();
+    }
+
+    public Integer getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(Integer expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getThirdToken() {
+        return thirdToken;
+    }
+
+    public void setThirdToken(String thirdToken) {
+        this.thirdToken = thirdToken == null ? null : thirdToken.trim();
+    }
+
+    public boolean isNewUser() {
+        return isNewUser;
+    }
+
+    public void setNewUser(boolean newUser) {
+        isNewUser = newUser;
+    }
+}
