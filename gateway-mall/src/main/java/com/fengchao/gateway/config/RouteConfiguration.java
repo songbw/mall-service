b5cc381c152ccf785c4ac9be6cee33da0bb7ed3f
song bingwei <bingwei.song@weesharing.com>
2019-09-02 10:44:12 +0800

    gateway noAuthorization config

--- gateway-mall/src/main/java/com/fengchao/gateway/config/RouteConfiguration.java
+++ gateway-mall/src/main/java/com/fengchao/gateway/config/RouteConfiguration.java
@@ -1,7 +1,5 @@
 package com.fengchao.gateway.config;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
