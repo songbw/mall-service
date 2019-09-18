001c21a9f1fc65f6df9eeb5d34663d675c0c6cf8
song bingwei <bingwei.song@weesharing.com>
2019-09-18 18:39:22 +0800

    del test

--- pingan-client/src/main/java/com/fengchao/pingan/utils/HttpClient.java
+++ pingan-client/src/main/java/com/fengchao/pingan/utils/HttpClient.java
@@ -1,24 +1,14 @@
 package com.fengchao.pingan.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fengchao.pingan.bean.InitCodeRequestBean;
-import org.apache.commons.beanutils.BeanMap;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import java.util.Date;
 
 public class HttpClient {
 
-    //    public static final String PINGAN_PATH = "http://citycard-stg.pingan.com.cn";
     public static final String PINGAN_PATH = "http://119.3.111.161:8088/v1";
-    public static final String PINGAN_AUTH_PATH = "http://basesmt-caas.yun.city.pingan.com/api/auth/openPlatform" ;
 
     public static final String ACCESS_TOKEN = "http://citycard-stg.pingan.com.cn/open-front/api/accessToken";
 
@@ -51,10 +41,6 @@ public class HttpClient {
     public static Client createClient() {
         Client client = ClientBuilder.newClient();
         return client;
-    }
-
-    public static void main(String args[]) {
-
     }
 
 }
