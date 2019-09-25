df5cde8cff3f238f5df5b4c2e39b48135ce7e788
song bingwei <bingwei.song@weesharing.com>
2019-09-25 15:06:31 +0800

    cdn

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/CosUtil.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/CosUtil.java
@@ -2,5 +2,5 @@ package com.fengchao.product.aoyi.utils;
 
 public class CosUtil {
     public static String baseAoyiProdUrl = "http://aoyiprod-1252099010.cossh.myqcloud.com/";
-    public static String iWalletUrlT = "https://iwallet-1258175138.cos.ap-beijing.myqcloud.com";
+    public static String iWalletUrlT = "http://iwallet-1258175138.file.myqcloud.com";
 }
