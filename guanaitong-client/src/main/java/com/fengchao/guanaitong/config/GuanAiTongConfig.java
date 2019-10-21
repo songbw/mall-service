b40b3dd4655ca65dd7fa29dee242997c64b173bd
Clark <1554711952@qq.com>
2019-10-21 12:14:42 +0800

    move url-prefix for guanaitong from source code to cofig-file

--- guanaitong-client/src/main/java/com/fengchao/guanaitong/config/GuanAiTongConfig.java
+++ guanaitong-client/src/main/java/com/fengchao/guanaitong/config/GuanAiTongConfig.java
@@ -15,14 +15,20 @@ public class GuanAiTongConfig implements InitializingBean {
     @Value("${GAT_APP_SECRET}")
     public  String GAT_APP_SECRET;
 
+    @Value("${GAT_URL_PREFIX}")
+    public  String GAT_URL_PREFIX;
+
     private static String CONFIG_GAT_APP_ID;
     private static String CONFIG_GAT_APP_SECRET;
+    private static String CONFIG_GAT_URL_PREFIX;
 
     @Override
     public void afterPropertiesSet() {
         CONFIG_GAT_APP_ID = GAT_APP_ID;
         CONFIG_GAT_APP_SECRET = GAT_APP_SECRET;
-        log.info("==== get guan_ai_tong config appId={}, appSecret={}",CONFIG_GAT_APP_ID,CONFIG_GAT_APP_SECRET);
+        CONFIG_GAT_URL_PREFIX = GAT_URL_PREFIX;
+        log.info("==== get guan_ai_tong config appId={}, appSecret={}, url={}",
+                CONFIG_GAT_APP_ID,CONFIG_GAT_APP_SECRET,CONFIG_GAT_URL_PREFIX);
     }
 
     public static String getAppId() {
@@ -31,6 +37,10 @@ public class GuanAiTongConfig implements InitializingBean {
 
     public static String getAppSecret() {
         return CONFIG_GAT_APP_SECRET;
+    }
+
+    public static String getConfigGatUrlPrefix() {
+        return CONFIG_GAT_URL_PREFIX;
     }
 }
 
