af4344b36641dfb46f13ca1457179d79b1ce0e8d
song bingwei <bingwei.song@weesharing.com>
2019-10-28 11:30:09 +0800

    fix bug

--- product-aoyi/src/test/java/com/fengchao/product/aoyi/ProductAoyiApplicationTests.java
+++ product-aoyi/src/test/java/com/fengchao/product/aoyi/ProductAoyiApplicationTests.java
@@ -1,18 +1,14 @@
 package com.fengchao.product.aoyi;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.fengchao.product.aoyi.bean.OperaResult;
-import com.fengchao.product.aoyi.bean.VendorsProfileBean;
-import com.fengchao.product.aoyi.constants.ProductStatusEnum;
+import com.fengchao.product.aoyi.bean.OperaResponse;
 import com.fengchao.product.aoyi.dao.ProductDao;
 import com.fengchao.product.aoyi.exception.ProductException;
 import com.fengchao.product.aoyi.feign.VendorsServiceClient;
-import com.fengchao.product.aoyi.mapper.SkuCodeMapper;
 import com.fengchao.product.aoyi.mapper.SkuCodeXMapper;
-import com.fengchao.product.aoyi.model.*;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import com.fengchao.product.aoyi.model.AoyiProdIndex;
+import com.fengchao.product.aoyi.model.AoyiProdIndexWithBLOBs;
+import com.fengchao.product.aoyi.model.SkuCode;
+import com.fengchao.product.aoyi.rpc.extmodel.SysCompany;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -50,20 +45,29 @@ public class ProductAoyiApplicationTests {
 			SkuCode skuCode = skuCodeMapper.selectByMerchantId(aoyiProdIndex.getMerchantId());
 			if (skuCode == null) {
 				// 查询商户信息
-				VendorsProfileBean profileBean = findVendorInfo(aoyiProdIndex.getMerchantId());
-				if (profileBean != null) {
-					// 获取最新code
-					SkuCode lastCode = skuCodeMapper.selectLast();
-					int code = Integer.parseInt(lastCode.getMerchantCode()) + 1;
-					skuCode = new SkuCode();
-					skuCode.setMerchantId(aoyiProdIndex.getMerchantId());
-					skuCode.setMerchantName(profileBean.getCompany().getName());
-					skuCode.setMerchantCode(code + "");
-					skuCode.setSkuValue(0);
-					skuCode.setCreatedAt(new Date());
-					skuCode.setUpdatedAt(new Date());
-					skuCodeMapper.insertSelective(skuCode);
+				OperaResponse<SysCompany> vendorResponse = vendorsService.vendorInfo(aoyiProdIndex.getMerchantId());
+				if (vendorResponse.getCode() == 200) {
+					SysCompany profileBean = vendorResponse.getData() ;
+					if (profileBean != null) {
+						// 获取最新code
+						SkuCode lastCode = skuCodeMapper.selectLast();
+						// logger.info(aoyiProdIndexX.getMerchantId() + "");
+						int code = Integer.parseInt(lastCode.getMerchantCode()) + 1;
+						// 添加商户信息
+						skuCode = new SkuCode();
+						skuCode.setMerchantId(aoyiProdIndex.getMerchantId());
+						skuCode.setMerchantName(profileBean.getName());
+						skuCode.setMerchantCode(code + "");
+						skuCode.setSkuValue(0);
+						skuCode.setCreatedAt(new Date());
+						skuCode.setUpdatedAt(new Date());
+						skuCodeMapper.insertSelective(skuCode);
+					} else {
+//						logger.warn("未获取到商户信息 merchantId:{}信息为:{}", aoyiProdIndex.getMerchantId());
+						throw new ProductException(200001, "vendor 服务失败 " + vendorResponse.getMsg());
+					}
 				} else {
+//					logger.warn("未获取到商户信息 merchantId:{}信息为:{}", aoyiProdIndex.getMerchantId());
 					throw new ProductException(200001, "商户信息为null");
 				}
 			}
@@ -82,17 +86,6 @@ public class ProductAoyiApplicationTests {
 			skuCode.setSkuValue(atomicInteger.get());
 			skuCodeMapper.updateSkuValueByPrimaryKey(skuCode);
 		});
-	}
-
-	private VendorsProfileBean findVendorInfo(int id) {
-		OperaResult result = vendorsService.vendorInfo(id) ;
-		if (result.getCode() == 200) {
-			Object object = result.getData() ;
-			String jsonString = JSON.toJSONString(object);
-			VendorsProfileBean profileBean = JSONObject.parseObject(jsonString, VendorsProfileBean.class);
-			return profileBean;
-		}
-		return null;
 	}
 
 }
