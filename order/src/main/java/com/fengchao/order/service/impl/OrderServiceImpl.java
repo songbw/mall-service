9acb0e1b61ca14036087b5bbc6330879ddacd99f
song bingwei <bingwei.song@weesharing.com>
2019-10-31 15:38:56 +0800

    log

--- order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
@@ -465,9 +465,10 @@ public class OrderServiceImpl implements OrderService {
                 inventoryMpus.setRemainNum(orderDetail1.getNum());
                 inventoryMpuses.add(inventoryMpus) ;
             });
+            logger.info("取消订单，返还库存操作失败，返还参数：{}", JSONUtil.toJsonString(inventoryMpuses));
             OperaResult result = productService.inventoryAdd(inventoryMpuses) ;
             if (result.getCode() != 200) {
-                logger.info("取消订单，返还库存操作失败，返还参数：{}", JSONUtil.toJsonString(inventoryMpuses), " 返回结果：{}", JSONUtil.toJsonString(result));
+                logger.info("取消订单，返还库存操作失败， 返回结果：{}", JSONUtil.toJsonString(result));
             }
         }
         return id;
