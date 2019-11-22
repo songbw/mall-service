099a789c4cefc36654b0b9a0385531baa1f8844f
song bingwei <bingwei.song@weesharing.com>
2019-11-23 00:35:07 +0800

    member

--- order/src/main/java/com/fengchao/order/dao/OldOrderDao.java
+++ order/src/main/java/com/fengchao/order/dao/OldOrderDao.java
@@ -1,12 +1,10 @@
 package com.fengchao.order.dao;
 
 import com.fengchao.order.bean.OldOrderQueryBean;
+import com.fengchao.order.mapper.ImsMcMembersMapper;
 import com.fengchao.order.mapper.ImsSupermanMallOrderItemMapper;
 import com.fengchao.order.mapper.ImsSupermanMallOrderMapper;
-import com.fengchao.order.model.ImsSupermanMallOrder;
-import com.fengchao.order.model.ImsSupermanMallOrderExample;
-import com.fengchao.order.model.ImsSupermanMallOrderItem;
-import com.fengchao.order.model.ImsSupermanMallOrderItemExample;
+import com.fengchao.order.model.*;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
@@ -26,11 +24,13 @@ public class OldOrderDao {
 
     private ImsSupermanMallOrderMapper imsSupermanMallOrderMapper ;
     private ImsSupermanMallOrderItemMapper imsSupermanMallOrderItemMapper ;
+    private ImsMcMembersMapper imsMcMembersMapper ;
 
     @Autowired
-    public OldOrderDao(ImsSupermanMallOrderMapper imsSupermanMallOrderMapper, ImsSupermanMallOrderItemMapper imsSupermanMallOrderItemMapper) {
+    public OldOrderDao(ImsSupermanMallOrderMapper imsSupermanMallOrderMapper, ImsSupermanMallOrderItemMapper imsSupermanMallOrderItemMapper, ImsMcMembersMapper imsMcMembersMapper) {
         this.imsSupermanMallOrderMapper = imsSupermanMallOrderMapper;
         this.imsSupermanMallOrderItemMapper = imsSupermanMallOrderItemMapper;
+        this.imsMcMembersMapper = imsMcMembersMapper ;
     }
 
     /**
@@ -40,22 +40,21 @@ public class OldOrderDao {
      * @return
      */
     public PageInfo<ImsSupermanMallOrder> selectAllPageable(OldOrderQueryBean queryBean) {
-        ImsSupermanMallOrderExample imsSupermanMallOrderExample = new ImsSupermanMallOrderExample();
-
-        ImsSupermanMallOrderExample.Criteria criteria = imsSupermanMallOrderExample.createCriteria();
-        criteria.andMobileEqualTo(queryBean.getMobile());
-
-        PageHelper.startPage(queryBean.getPageNo(), queryBean.getPageSize());
-
+        ImsMcMembers imsMcMembers = selectMembersByMobile(queryBean.getMobile()) ;
         List<ImsSupermanMallOrder> ordersList = new ArrayList<>() ;
-        imsSupermanMallOrderMapper.selectByExample(imsSupermanMallOrderExample).forEach(imsSupermanMallOrder -> {
-            List<ImsSupermanMallOrderItem> items = new ArrayList<>() ;
-            items = selectItemByOrderId(imsSupermanMallOrder.getId()) ;
-            imsSupermanMallOrder.setItemList(items);
-            ordersList.add(imsSupermanMallOrder) ;
-        });
+        if (imsMcMembers != null && imsMcMembers.getUid() != null) {
+            ImsSupermanMallOrderExample imsSupermanMallOrderExample = new ImsSupermanMallOrderExample();
+            ImsSupermanMallOrderExample.Criteria criteria = imsSupermanMallOrderExample.createCriteria();
+            criteria.andUidEqualTo(imsMcMembers.getUid());
+            PageHelper.startPage(queryBean.getPageNo(), queryBean.getPageSize());
+            imsSupermanMallOrderMapper.selectByExample(imsSupermanMallOrderExample).forEach(imsSupermanMallOrder -> {
+                List<ImsSupermanMallOrderItem> items = new ArrayList<>() ;
+                items = selectItemByOrderId(imsSupermanMallOrder.getId()) ;
+                imsSupermanMallOrder.setItemList(items);
+                ordersList.add(imsSupermanMallOrder) ;
+            });
+        }
         PageInfo<ImsSupermanMallOrder> pageInfo = new PageInfo(ordersList);
-
         return pageInfo;
     }
 
@@ -65,5 +64,16 @@ public class OldOrderDao {
         criteria.andOrderidEqualTo(orderId) ;
         List<ImsSupermanMallOrderItem> items = imsSupermanMallOrderItemMapper.selectByExample(imsSupermanMallOrderItemExample) ;
         return items ;
+    }
+
+    public ImsMcMembers selectMembersByMobile(String mobile) {
+        ImsMcMembersExample example = new ImsMcMembersExample() ;
+        ImsMcMembersExample.Criteria criteria = example.createCriteria() ;
+        criteria.andMobileEqualTo(mobile) ;
+        List<ImsMcMembers> items = imsMcMembersMapper.selectByExample(example) ;
+        if (items != null && items.size() > 0) {
+            return items.get(0) ;
+        }
+        return null;
     }
 }
