1a1b8742c308256c8dfb94c931e783295e64decf
lufei <1808319164@qq.com>
2019-11-26 15:48:12 +0800

    add promotion tag

---
+++ equity/src/main/java/com/fengchao/equity/dao/PromotionTagsDao.java
@@ -0,0 +1,48 @@
+package com.fengchao.equity.dao;
+
+import com.fengchao.equity.mapper.PromotionTagsMapper;
+import com.fengchao.equity.model.PromotionTags;
+import com.fengchao.equity.model.PromotionTagsExample;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PromotionTagsDao {
+
+    @Autowired
+    private PromotionTagsMapper mapper;
+
+    public int createPromotionTags(PromotionTags bean) {
+        return mapper.insertSelective(bean);
+    }
+
+    public int deletePromotionTags(Integer id) {
+        PromotionTags promotionTags = new PromotionTags();
+        promotionTags.setId(id);
+
+        PromotionTagsExample example = new PromotionTagsExample();
+        PromotionTagsExample.Criteria criteria = example.createCriteria();
+
+        criteria.andIdEqualTo(id);
+        criteria.andIsStatusEqualTo(2);
+        return mapper.updateByExampleSelective(promotionTags, example);
+    }
+
+    public int updatePromotionTags(PromotionTags bean) {
+        return mapper.updateByPrimaryKeySelective(bean);
+    }
+
+    public PageInfo<PromotionTags> findPromotionTags(Integer pageNo, Integer pageSize) {
+        PromotionTagsExample example = new PromotionTagsExample();
+        PromotionTagsExample.Criteria criteria = example.createCriteria();
+
+        criteria.andIsStatusEqualTo(1);
+        PageHelper.startPage(pageNo, pageSize);
+        List<PromotionTags> promotionTags = mapper.selectByExample(example);
+        return new PageInfo<>(promotionTags);
+    }
+}
---
+++ equity/src/main/java/com/fengchao/equity/service/impl/PromotionTagsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.fengchao.equity.service.impl;
+
+import com.fengchao.equity.bean.page.PageableData;
+import com.fengchao.equity.bean.vo.PageVo;
+import com.fengchao.equity.dao.PromotionTagsDao;
+import com.fengchao.equity.model.PromotionTags;
+import com.fengchao.equity.service.PromotionTagsService;
+import com.fengchao.equity.utils.ConvertUtil;
+import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class PromotionTagsServiceImpl implements PromotionTagsService {
+
+    @Autowired
+    private PromotionTagsDao tagsDao;
+
+    @Override
+    public int createPromotionTags(PromotionTags bean) {
+        return tagsDao.createPromotionTags(bean);
+    }
+
+    @Override
+    public PageableData<PromotionTags> findPromotionTags(Integer pageNo, Integer pageSize) {
+        PageableData<PromotionTags> pageableData = new PageableData<>();
+        PageInfo<PromotionTags> pageInfo = tagsDao.findPromotionTags(pageNo, pageSize);
+
+        PageVo pageVo = ConvertUtil.convertToPageVo(pageInfo);
+        List<PromotionTags> groupInfoList = pageInfo.getList();
+        pageableData.setList(groupInfoList);
+        pageableData.setPageInfo(pageVo);
+        return pageableData;
+    }
+
+    @Override
+    public int updatePromotionTags(PromotionTags bean) {
+        return tagsDao.updatePromotionTags(bean);
+    }
+
+    @Override
+    public int deletePromotionTags(Integer id) {
+        return tagsDao.deletePromotionTags(id);
+    }
+}
