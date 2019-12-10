53ad18a06746814e525169f784fc58c44065eb74
tom jing <tom@toms-MBP.lan>
2019-12-10 12:00:21 +0800

    v

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/controller/PlatformController.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/controller/PlatformController.java
@@ -1,48 +1,23 @@
 package com.fengchao.product.aoyi.controller;
 
 import com.fengchao.product.aoyi.bean.OperaResponse;
-import com.fengchao.product.aoyi.bean.QueryBean;
-import com.fengchao.product.aoyi.model.Platform;
+import com.fengchao.product.aoyi.bean.OperaResult;
+import com.fengchao.product.aoyi.service.BrandService;
 import com.fengchao.product.aoyi.service.PlatformService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author songbw
- * @date 2019/11/22 10:54
- */
+
 @RestController
 @RequestMapping(value = "/platform", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class PlatformController {
 
     @Autowired
-    private PlatformService service ;
-    @PostMapping("/all")
-    private OperaResponse findList(@RequestBody QueryBean queryBean) {
-        return service.findList(queryBean);
-    }
-
-    @GetMapping
-    private OperaResponse find(Integer id) {
-        return service.find(id);
-    }
-
-    @PostMapping
-    private OperaResponse add(@RequestBody Platform bean) {
-        return service.add(bean);
-    }
-
-    @DeleteMapping
-    private OperaResponse delete(Integer id) {
-        return service.delete(id);
-    }
-
-    @PutMapping
-    private OperaResponse modify(@RequestBody Platform bean) {
-        return service.modify(bean);
-    }
+    private PlatformService service;
 
     @GetMapping
     private OperaResponse find(String appId) {
