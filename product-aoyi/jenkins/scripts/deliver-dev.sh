48e841b12e270f24fbdaffb6d401934da069e4a2
三角寒冰 <sbw227@163.com>
2020-02-03 10:59:34 +0800

    reset

--- product-aoyi/jenkins/scripts/deliver-dev.sh
+++ product-aoyi/jenkins/scripts/deliver-dev.sh
@@ -9,19 +9,6 @@ set tag_dir /data/server/product/userapps
 set name product-aoyi-0.0.1-SNAPSHOT.jar
 set tmp_dir /tmp
 
-send "/usr/local/mysql/bin/mysqldump -u fengchao -p -d fc_mall > db.sql\n"
-expect "password:"
-send "${passwd}\n"
-set timeout 30000
-expect "$ "
-
-##拷贝db文件到目标机器
-spawn sh -c " scp -P 22051 -r db.sql $user@121.36.52.130:$tmp_dir\n"
-expect "password:"
-send "${passwd}\n"
-set timeout 30000
-expect "$ "
-
 ##拷贝jar文件到目标机器
 spawn sh -c " scp -P $port -r $src_dir$name $user@$host:$tag_dir"
 expect "password:"
