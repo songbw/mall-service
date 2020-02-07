4b82819a860bbad28324680052c70a995c38fc8e
Your Name <you@example.com>
2020-02-07 10:25:18 +0800

    v

--- product-aoyi/src/test/java/com/fengchao/product/aoyi/WeipinhuiTests.java
+++ product-aoyi/src/test/java/com/fengchao/product/aoyi/WeipinhuiTests.java
@@ -2,6 +2,7 @@ package com.fengchao.product.aoyi;
 
 import com.fengchao.product.aoyi.service.weipinhui.WeipinhuiDataService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,10 @@ public class WeipinhuiTests {
 	private WeipinhuiDataService weipinhuiDataService;
 
 	@Test
+	@Ignore
 	public void testWeipinhui() {
 		try {
-			weipinhuiDataService.getBrand(1, -1);
+			weipinhuiDataService.getBrand(2, 1);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
