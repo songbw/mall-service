7ef02b6c0fde15942e3ae2f335a4c03a8b8dd33a
三角寒冰 <sbw227@163.com>
2020-02-10 12:22:40 +0800

    4

--- order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/fengchao/order/service/impl/OrderServiceImpl.java
@@ -238,7 +238,7 @@ public class OrderServiceImpl implements OrderService {
                     return operaResult;
                 }
                 // 添加扣除库存列表
-                if (orderSku.getMerchantId() != 2) {
+                if (orderSku.getMerchantId() != 2 && orderSku.getMerchantId() != 4) {
                     InventoryMpus inventoryMpus = new InventoryMpus();
                     inventoryMpus.setMpu(orderSku.getMpu());
                     inventoryMpus.setRemainNum(orderSku.getNum());
