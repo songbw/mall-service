350d4d9ff85dac28a09dc9d7a6e2904734cf69bf
三角寒冰 <sbw227@163.com>
2020-02-10 11:40:12 +0800

    sku tongbu

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/AsyncTask.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/utils/AsyncTask.java
@@ -159,22 +159,22 @@ public class AsyncTask {
                         StarSku skuBean = JSON.parseObject(jsonObject.toJSONString(), new TypeReference<StarSku>(){});
                         skuBean.setSpuId(spuArray.getString(i));
                         // insert spu
-                        List<AoyiProdIndex> aoyiProdIndices =  productDao.findBySkuId(spuArray.getString(i), 4) ;
-                        if (aoyiProdIndices == null || aoyiProdIndices.size() == 0) {
-                            starSkuMapper.insertSelective(skuBean) ;
-                            JSONArray propertyArray = jsonObject.getJSONArray("skuPropertyList") ;
-                            for (int j = 0; j < propertyArray.size(); j++) {
-                                JSONObject propertyJson = propertyArray.getJSONObject(j) ;
-                                StarProperty starProperty  = JSON.parseObject(propertyJson.toJSONString(), new TypeReference<StarProperty>(){});
-                                // set spu id
-                                starProperty.setProductId(skuBean.getId());
-                                // set type 1
-                                starProperty.setType(1);
-                                // isert property
-                                starPropertyMapper.insertSelective(starProperty) ;
-                            }
-                            log.info("获取SKU信息，入参：{}, 结果：{}",spuArray.getString(i), JSONUtil.toJsonString(skuBean));
+//                        List<AoyiProdIndex> aoyiProdIndices =  productDao.findBySkuId(spuArray.getString(i), 4) ;
+//                        if (aoyiProdIndices == null || aoyiProdIndices.size() == 0) {
+                        starSkuMapper.insertSelective(skuBean) ;
+                        JSONArray propertyArray = jsonObject.getJSONArray("skuPropertyList") ;
+                        for (int j = 0; j < propertyArray.size(); j++) {
+                            JSONObject propertyJson = propertyArray.getJSONObject(j) ;
+                            StarProperty starProperty  = JSON.parseObject(propertyJson.toJSONString(), new TypeReference<StarProperty>(){});
+                            // set spu id
+                            starProperty.setProductId(skuBean.getId());
+                            // set type 1
+                            starProperty.setType(1);
+                            // isert property
+                            starPropertyMapper.insertSelective(starProperty) ;
                         }
+                        log.info("获取SKU信息，入参：{}, 结果：{}",spuArray.getString(i), JSONUtil.toJsonString(skuBean));
+//                        }
                     }
                 }
             }
