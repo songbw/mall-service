76e1c716f8ab36c936d6752abf6de6fb389278a6
三角寒冰 <sbw227@163.com>
2020-02-25 18:00:49 +0800

    integer price

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/bean/SerachBean.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/bean/SerachBean.java
@@ -19,6 +19,6 @@ public class SerachBean {
     private Integer id;
     private String order;
     private String mpu;
-    private String minPrice = "0";
-    private String maxPrice = "10000";
+    private Integer minPrice;
+    private Integer maxPrice;
 }
