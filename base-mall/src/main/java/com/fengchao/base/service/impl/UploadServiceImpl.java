7dd0f6a0285ea41f59e8d323f94a08b9e35da2ed
Your Name <you@example.com>
2020-03-07 14:58:30 +0800

    v

--- base-mall/src/main/java/com/fengchao/base/service/impl/UploadServiceImpl.java
+++ base-mall/src/main/java/com/fengchao/base/service/impl/UploadServiceImpl.java
@@ -46,12 +46,14 @@ public class UploadServiceImpl implements UploadService {
         // String fileName = array1[1];
         String fileName = img.getAyImage().substring(img.getAyImage().lastIndexOf("/") + 1);
         try {
-            log.info("下载上传图片 url:{}, fileName:{}, savePath:{}",
+            log.info("下载上传图片 保存到本地 url:{}, fileName:{}, savePath:{}",
                     img.getAyImage(), fileName, base + img.getPath() + img.getType());
             URLConnectionDownloader.download(img.getAyImage(), fileName, base + img.getPath() + img.getType());
         } catch (Exception e) {
             log.error("下载上传图片异常:{}", e.getMessage(), e);
         }
+        log.info("下载上传图片 本地路径:{} 上传路径:{}",
+                base + img.getPath() + img.getType() + fileName, img.getPath() + img.getType() + fileName);
         CosUtil.upload(CosUtil.iWalletBucketName, new File(base + img.getPath() + img.getType() + fileName),img.getPath() + img.getType() + fileName) ;
         productService.imageBack(img.getId(), 1) ;
     }
