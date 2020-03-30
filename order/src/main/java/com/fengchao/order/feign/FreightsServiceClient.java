34770e66964e688c5b93045925caef6eb49e7ccc
Your Name <you@example.com>
2020-03-30 16:28:55 +0800

    v

--- order/src/main/java/com/fengchao/order/feign/FreightsServiceClient.java
+++ order/src/main/java/com/fengchao/order/feign/FreightsServiceClient.java
@@ -20,7 +20,7 @@ public interface FreightsServiceClient {
      * @param idList
      * @return
      */
-    @RequestMapping(value = "/freight/adminShip/findIdList", method = RequestMethod.GET)
+    @RequestMapping(value = "/freights/adminShip/findIdList", method = RequestMethod.GET)
     OperaResponse<List<ShipTemplateBean>> queryMerchantExceptionFee(@RequestParam("idList") String idList);
 
 }
