fd46b82d1c4f3e55e8ee3f01059a3478f13a6f92
Your Name <you@example.com>
2020-03-30 15:48:08 +0800

    v

--- order/src/main/java/com/fengchao/order/controller/ExportStatisticController.java
+++ order/src/main/java/com/fengchao/order/controller/ExportStatisticController.java
@@ -48,7 +48,7 @@ public class ExportStatisticController {
      * @param response
      * @throws Exception
      */
-    @GetMapping(value = "/settelment")
+    @GetMapping(value = "/settlement")
     public void exportSettlement(@RequestParam("startTime") String startTime,
                             @RequestParam("endTime") String endTime,
                             @RequestParam("merchantId") Integer merchantId,
