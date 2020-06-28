d977764ff69c4f50848a9a7a90f5a7864404b5fe
song bingwei <bingwei.song@weesharing.com>
2020-06-28 18:09:38 +0800

    nickname

--- sso/src/main/java/com/fengchao/sso/bean/AuthUserBean.java
+++ sso/src/main/java/com/fengchao/sso/bean/AuthUserBean.java
@@ -5,8 +5,6 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
 public class AuthUserBean implements Serializable{
     private int realNameStatus;
     private int marry;
@@ -29,4 +27,172 @@ public class AuthUserBean implements Serializable{
     private String nickName;
     private String nickname;
     private String payId;
+
+    public int getRealNameStatus() {
+        return realNameStatus;
+    }
+
+    public void setRealNameStatus(int realNameStatus) {
+        this.realNameStatus = realNameStatus;
+    }
+
+    public int getMarry() {
+        return marry;
+    }
+
+    public void setMarry(int marry) {
+        this.marry = marry;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(String idcard) {
+        this.idcard = idcard;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVolk() {
+        return volk;
+    }
+
+    public void setVolk(String volk) {
+        this.volk = volk;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getCensus() {
+        return census;
+    }
+
+    public void setCensus(String census) {
+        this.census = census;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPayId() {
+        return payId;
+    }
+
+    public void setPayId(String payId) {
+        this.payId = payId;
+    }
 }
