2a03a0121adef2cd52571d8f93a67f0d7c235d71
song bingwei <bingwei.song@weesharing.com>
2020-10-12 14:30:28 +0800

    state

--- product-aoyi/src/main/java/com/fengchao/product/aoyi/service/impl/AppSkuStateServiceImpl.java
+++ product-aoyi/src/main/java/com/fengchao/product/aoyi/service/impl/AppSkuStateServiceImpl.java
@@ -108,50 +108,54 @@ public class AppSkuStateServiceImpl implements AppSkuStateService {
     public OperaResponse updateBatchState(List<AppSkuState> beans) {
         OperaResponse response = new OperaResponse() ;
         /**
-         * 分四种情况
-         * 1、主商品为上架状态，子商品也是上架状态（状态相同，不处理）
-         * 2、主商品为下架状态，子商品也是下架状态（状态相同，不处理）
-         * 3、主商品为上架状态，子商品为下架状态（添加一条子商品为下架状态的记录）
-         * 4、主商品为下架状态，子商品为上架状态（子商品记录删除）
+         * 分12种情况
+         * 1、主商品为上架状态，子商品不存在，请求也是上架状态（状态相同，不处理）
+         * 2、主商品为下架状态，子商品不存在，请求也是下架状态（状态相同，不处理）
+         * 3、主商品为上架状态，子商品不存在，请求为下架状态（添加一条子商品为下架状态的记录）
+         * 4、主商品为下架状态，子商品不存在，请求为上架状态（不处理）
+         * 5、主商品为上架状态，子商品上架状态，请求也是上架状态（状态相同，不处理）111
+         * 6、主商品为下架状态，子商品下架状态，请求也是下架状态（状态相同，不处理）000
+         * 7、主商品为上架状态，子商品下架状态，请求为下架状态（不处理）100
+         * 8、主商品为下架状态，子商品上架状态，请求为上架状态（删除）011
+         * 9、主商品为下架状态，子商品下架状态，请求也是上架状态（不处理）001
+         * 10、主商品为下架状态，子商品上架状态，请求也是下架状态（删除）010
+         * 11、主商品为上架状态，子商品下架状态，请求也是上架状态（删除）101
+         * 12、主商品为上架状态，子商品上架状态，请求也是下架状态（更新为下架）110
          */
         Date date = new Date();
         beans.forEach(bean -> {
             // 判断添加，还是删除
             int checkState = 0 ;
+            List<AppSkuState> appSkuStates = dao.selectByRenterIdAndMpuAndSku(bean) ;
+            AppSkuState appSkuState = null;
+            if (appSkuStates != null && appSkuStates.size() > 0) {
+                appSkuState = appSkuStates.get(0) ;
+            }
             if (bean.getMpu().equals(bean.getSkuId())) {
                 AoyiProdIndex aoyiProdIndex = productDao.selectByMpu(bean.getMpu()) ;
                 if (aoyiProdIndex != null) {
-                    checkState = checkState(Integer.parseInt(aoyiProdIndex.getState()), bean.getState()) ;
+                    checkState = checkState(Integer.parseInt(aoyiProdIndex.getState()), bean.getState(), appSkuState) ;
                 }
             } else {
                 List<StarSku> starSkus = starSkuDao.selectBySpuIdAndCode(bean.getMpu(), bean.getSkuId()) ;
                 if (starSkus != null && starSkus.size() > 0) {
                     StarSku starSku = starSkus.get(0) ;
-                    checkState = checkState(starSku.getStatus(), bean.getState()) ;
+                    checkState = checkState(starSku.getStatus(), bean.getState(), appSkuState) ;
                 }
             }
             if (checkState == 3) {
-                List<AppSkuState> appSkuStates = dao.selectByRenterIdAndMpuAndSku(bean) ;
-                if (appSkuStates != null && appSkuStates.size() > 0) {
-                    AppSkuState appSkuState = appSkuStates.get(0) ;
-                    // 更新
-                    if (appSkuState.getState() != bean.getState()) {
-                        bean.setUpdatedAt(date);
-                        mapper.updateByPrimaryKeySelective(bean) ;
-                    }
-                } else {
-                    // insert
-                    bean.setCreatedAt(date);
-                    bean.setUpdatedAt(date);
-                    mapper.insertSelective(bean) ;
-                }
+                // insert
+                bean.setCreatedAt(date);
+                bean.setUpdatedAt(date);
+                mapper.insertSelective(bean) ;
             }
-            if (checkState == 4) {
-                List<AppSkuState> appSkuStates = dao.selectByRenterIdAndMpuAndSku(bean) ;
-                if (appSkuStates != null && appSkuStates.size() > 0) {
-                    AppSkuState appSkuState = appSkuStates.get(0) ;
-                    mapper.deleteByPrimaryKey(appSkuState.getId()) ;
-                }
+            if (checkState == 8 || checkState == 10 || checkState == 11) {
+                mapper.deleteByPrimaryKey(appSkuState.getId()) ;
+            }
+            if (checkState == 12) {
+                // 更新
+                bean.setUpdatedAt(date);
+                mapper.updateByPrimaryKeySelective(bean) ;
             }
         });
         response.setData(beans);
@@ -160,18 +164,38 @@ public class AppSkuStateServiceImpl implements AppSkuStateService {
 
     /**
      * 上下架状态 0：下架；1：上架
-     * 分四种情况
-     * 1、主商品为上架状态，子商品也是上架状态（状态相同，不处理）
-     * 2、主商品为下架状态，子商品也是下架状态（状态相同，不处理）
-     * 3、主商品为上架状态，子商品为下架状态（添加一条子商品为下架状态的记录）
-     * 4、主商品为下架状态，子商品为上架状态（子商品记录删除）
+     * 分12种情况
+     * 1、主商品为上架状态，子商品不存在，请求也是上架状态（状态相同，不处理）
+     * 2、主商品为下架状态，子商品不存在，请求也是下架状态（状态相同，不处理）
+     * 3、主商品为上架状态，子商品不存在，请求为下架状态（添加一条子商品为下架状态的记录）
+     * 4、主商品为下架状态，子商品不存在，请求为上架状态（不处理）
+     * 5、主商品为上架状态，子商品上架状态，请求也是上架状态（状态相同，不处理）111
+     * 6、主商品为下架状态，子商品下架状态，请求也是下架状态（状态相同，不处理）000
+     * 7、主商品为上架状态，子商品下架状态，请求为下架状态（不处理）100
+     * 8、主商品为下架状态，子商品上架状态，请求为上架状态（删除）011
+     * 9、主商品为下架状态，子商品下架状态，请求也是上架状态（不处理）001
+     * 10、主商品为下架状态，子商品上架状态，请求也是下架状态（删除）010
+     * 11、主商品为上架状态，子商品下架状态，请求也是上架状态（删除）101
+     * 12、主商品为上架状态，子商品上架状态，请求也是下架状态（更新为下架）110
      */
-    private int checkState(int prodState, int renterState) {
-        if (prodState == 1 && renterState == 0) {
-            return 3;
-        }
-        if (prodState == 0 && renterState == 1) {
-            return 4;
+    private int checkState(int prodState, int renterState, AppSkuState bean) {
+        if (bean == null) {
+            if (prodState == 1 && renterState == 0) {
+                return 3;
+            }
+        } else {
+            if (prodState == 0 && bean.getState() == 1 && renterState == 1) {
+                return 8;
+            }
+            if (prodState == 0 && bean.getState() == 1 && renterState == 0) {
+                return 10;
+            }
+            if (prodState == 1 && bean.getState() == 0 && renterState == 1) {
+                return 11;
+            }
+            if (prodState == 1 && bean.getState() == 1 && renterState == 0) {
+                return 12;
+            }
         }
         return 0;
     }
