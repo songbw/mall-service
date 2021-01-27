3eb144279ac9d58b3cb17b6425246322d2d7ff9c
song bingwei <bingwei.song@weesharing.com>
2021-01-27 17:55:36 +0800

    fusion config

--- aoyi-client/src/main/java/com/fengchao/aoyi/client/config/AoyiClientConfig.java
+++ aoyi-client/src/main/java/com/fengchao/aoyi/client/config/AoyiClientConfig.java
@@ -9,6 +9,12 @@ public class AoyiClientConfig {
 
     private String gatUrl;
 
+    private String fusionBaseUrl ;
+    private String fusionAppKey ;
+    private String fusionToken ;
+    private String fusionKioskPath ;
+    private String fusionSoltPath ;
+
     public String getGatBackUrl() {
         return gatBackUrl;
     }
@@ -23,5 +29,45 @@ public class AoyiClientConfig {
 
     public void setGatUrl(String gatUrl) {
         this.gatUrl = gatUrl;
+    }
+
+    public String getFusionBaseUrl() {
+        return fusionBaseUrl;
+    }
+
+    public void setFusionBaseUrl(String fusionBaseUrl) {
+        this.fusionBaseUrl = fusionBaseUrl;
+    }
+
+    public String getFusionAppKey() {
+        return fusionAppKey;
+    }
+
+    public void setFusionAppKey(String fusionAppKey) {
+        this.fusionAppKey = fusionAppKey;
+    }
+
+    public String getFusionToken() {
+        return fusionToken;
+    }
+
+    public void setFusionToken(String fusionToken) {
+        this.fusionToken = fusionToken;
+    }
+
+    public String getFusionKioskPath() {
+        return fusionKioskPath;
+    }
+
+    public void setFusionKioskPath(String fusionKioskPath) {
+        this.fusionKioskPath = fusionKioskPath;
+    }
+
+    public String getFusionSoltPath() {
+        return fusionSoltPath;
+    }
+
+    public void setFusionSoltPath(String fusionSoltPath) {
+        this.fusionSoltPath = fusionSoltPath;
     }
 }
